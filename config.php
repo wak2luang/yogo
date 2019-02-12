7d3124de08076676326c6a86485f63e396a2a7a2
wak2luang <45333585+wak2luang@users.noreply.github.com>
2019-02-12 18:52:20 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config = [
-"imei" => ["356959080241117"],
-"sign" => ["fde626d730b7893bf43c3be58860fb7b4f254173"],
-"token" => ["a5540d073a19a895d6f1c7f858905f0d"],
-"uuid" => ["79e517530e48f4b1"],
+"imei" => ["xxxxxxxxxxxxxxx"],
+"sign" => ["xxxxxxxxxxxxxxx"],
+"token" => ["xxxxxxxxxxxxxxx"],
+"uuid" => ["xxxxxxxxxxxxxxxx"],
 ];
 ?>
