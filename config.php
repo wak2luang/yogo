99a6efbec6e0cc11f163066ed66f6fbc08618fd9
wak2luang <45333585+wak2luang@users.noreply.github.com>
2019-02-12 18:58:01 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,46 @@
+<?php
+// urutkan config nya kaya contoh dibawah akun ke 2/3 jg sama !!
+
+// config
+$config = [
+
+  "imei" => [
+// akun ke 1
+"xxxxxxx",
+// akun ke 2
+
+// akun ke 3
+
+
+],
+  "sign" => [
+// akun ke 1
+"xxxxxxx", 
+// akun ke 2
+
+// akun ke 3
+
+
+],
+  "token" => [
+// akun ke 1
+"xxxxxxxx", 
+// akun ke 2
+
+// akun ke 3
+
+
+],
+  "uuid" => [
+// akun ke 1
+"xxxxxxxx", 
+// akun ke 2
+
+// akun ke 3
+
+
+],
+
+  ];
+
+?>
